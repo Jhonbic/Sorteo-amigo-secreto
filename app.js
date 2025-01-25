52802cdafc3df44c92654725a0450df8abc98999
Anderson España <jhonlespa12@gmail.com>
2025-01-25 10:25:18 -0500

    Función de agregar amigo creada

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 const contenedor = document.getElementById('listaAmigos');
+const name = document.getElementById('amigo');
+
+const amigos = [];
+
+function agregarAmigo() {
+    if (name.value === '') {
+        alert('Debes escribir un nombre');   
+    }else{
+        const amigo = name.value;
+        amigos.push(amigo);
+        const li = document.createElement('li');
+        li.innerHTML = amigo;
+        contenedor.appendChild(li);
+        console.log(amigos);
+        name.value = '';
+    }
+}
